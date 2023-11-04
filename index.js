b73b7a8a2ed484e0e8bdacf4b0ee18a1a551c950
shahinaaktershima <shima421914@gmail.com>
2023-11-05 00:40:49 +0600

    setup-server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app=express();
+const port=process.env.PORT || 5000;
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('job server is running now');
+})
+app.listen(port,()=>{
+    console.log('job server is running on port',port);
+})
